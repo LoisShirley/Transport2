77795c87d2916b0bfb0cf45b7bd75b694b6ffa29
ShirleyLois <1542924158@qq.com>
2020-05-18 22:19:32 +0800

    Initial commit

---
+++ src/com/wmy/Transportation/AirTransport.java
@@ -0,0 +1,10 @@
+package com.wmy.Transportation;
+
+public class AirTransport extends Transport {
+
+    @Override
+    public void drivingMethod(String ownership, String GPSPosition, String price, String id, String type) {
+        super.drivingMethod(ownership, GPSPosition, price, id, type);
+    }
+
+}
